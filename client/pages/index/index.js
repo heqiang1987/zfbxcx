cf508e961901bccd84d9d43829e81cffb04a85da
heqiang <549233436@qq.com>
2019-03-28 20:54:21 +0800

    1

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -1,16 +1,27 @@
+var app = getApp();
 Page({
-
-  data: {},
-
+  data: {
+    imgUrls:["/images/flash.png","/images/flash.png","/images/flash.png"]
+  },
   // 初始化页面显示正在加载（因为需要异步加载用户信息）
   onLoad() {
-  
+     var t = this;
+     app.request({
+      url:"index/home1"
+     },function(res){
+         console.log(res);
+         if(res.status){
+           var result = res.result;
+           t.setData({
+             ad:result.ad,
+             jdgoods:result.jdgoods.list,
+             jjgoods:result.jjgoods.list
+           })
+         }
+        console.log("qqqqqqqqqqq"); 
+        console.log(t.data);
+     })
   },
 
-  // 显示指定当前用户的图片列表
-  onShow() {
-   
-  }
 
- 
 });
