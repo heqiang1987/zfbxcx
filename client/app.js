1c53b716529cfab3d21f17211cdfb1463b2e7909
heqiang <549233436@qq.com>
2019-04-04 17:59:10 +0800

    qq

--- client/app.js
+++ client/app.js
@@ -1,6 +1,7 @@
 App({
   onLaunch() {
     //console.log("22222222222222222222")
+    this.setCache("uid",7044);
     my.getAuthCode({
      scopes: 'auth_user',
      success: (res) => {
@@ -19,6 +20,7 @@ App({
     });
      },
    });
+
   },
   onShow(){
   },
@@ -41,28 +43,52 @@ App({
                fn(res.data)
             },
             fail: function (res) {
-                // console.log('fail');
-                // console.log(res);
-                // if (object.fail) {
-                //     object.fail(res);
-                // }
             },
             complete: function (res) {
-                // console.log('com');
-                // console.log(res);
-                // if (object.complete) {
-                //     object.complete(res);
-                // }
             }
         });
 
   }, 
-  globalData: {
-    appid: "wxb0552d591a5755f1",//新零售长策技术，不需要改先版本号了
-    api: "https://lgxn.hq87.top/",
-    o2o: true,
-    userInfo: null,
-    ver: 340
-  }
+  getCache: function(e, t) {
+        var i = +new Date / 1e3,
+            n = "";
+            i = parseInt(i);
+        try {
+            (n = my.getStorageSync(e + this.globalData.appid)).expire > i || 0 == n.expire ? n = n.value : (n = "", this.removeCache(e))
+        } catch (e) {
+            n = void 0 === t ? "" : t
+        }
+        return n = n || ""
+    },
+  setCache: function(e, t, i) {
+        var n = +new Date / 1e3,
+            a = !0,
+            o = {
+                expire: i ? n + parseInt(i) : 0,
+                value: t
+            };
+        try {
+            my.setStorageSync(e + this.globalData.appid, o)
+        } catch (e) {
+            a = !1
+        }
+        return a
+ },
+ removeCache: function(e) {
+        var t = !0;
+        try {
+            my.removeStorageSync(e + this.globalData.appid)
+        } catch (e) {
+            t = !1
+        }
+        return t
+},
+globalData: {
+  appid: "wxb0552d591a5755f1",//新零售长策技术，不需要改先版本号了
+  api: "https://lgxn.hq87.top/",
+  o2o: true,
+  userInfo: null,
+  ver: 340
+}
 
 });
